7090ec1849cf4dd24e829df526c125a45f079a3a
wyamet <wyattdomanski.md@gmail.com>
2023-01-14 18:50:53 -0800

    added resources to index.js

---
+++ main/index.js
@@ -0,0 +1,6 @@
+const Manager = require('./library/manager');
+const Engineer = require('./library/engineer');
+const Intern = require('./library/intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
